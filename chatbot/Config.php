0c7275e1d6660b5400af4e05e8b4161d5b796bbb
Akshay N Shaju <mail@akshaynshaju.com>
2018-05-30 22:31:22 +0530

    Add files via upload

---
+++ chatbot/Config.php
@@ -0,0 +1,53 @@
+<?php
+/*Developed By : Akshay N Shaju
+Developed On : 14/03/18
+Last Updated : --*/
+
+class Config
+{
+
+    public $log = false;
+    public $multiChatbot = true;
+
+    public $connectionInfo = array(
+        "host" => "localhost",
+        "user" => "akshay",
+        "pass" => "akshay123",
+        "dbName" => "eva-bot"
+    );
+
+    public $parserInfo = array(
+        "aimlDir" => "aiml"
+    );
+
+    public $botInfo =  array(
+            'name' => "Eva",
+            'age' => "22",
+            'birthday' => "22/04/1994",
+            'owner' => "Akshay",
+            'master' => "Akshay",
+            'botmaster' => "Akshay",
+            'species' => "bot developed by Akshay",
+            'years' => "1994",
+            'months' => "4",
+            'days' => "22",
+            'gender' => "computer",
+            'phone-number' => "+91 8129267093",
+            'location' => "Thrissur,Kerala,India",
+            'marital-status' => "single",
+            'website' => "www.Akshay.com",
+            'birthplace' => "Thrissur	",
+            'mother' => "Akshay",
+            'nationality' => "Indian",
+            'best-friend' => "Akshay",
+            'friends' => "Akshay",
+            'pets' => "Juggru, my cat",
+            'version' => "1.1"
+        );
+
+    public $userInfo = array(
+        "name" => "userName",
+        "age" => 20,
+    );
+
+}
